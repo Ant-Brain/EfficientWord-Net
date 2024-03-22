7cbfd936a29d903f3e7b3bbd3ced3305c3d60684
kiri <kiri0803@gmail.com>
2024-03-23 00:15:03 +0900

    - Update dependencies for Python 3.10+, drop 3.6-3.7 support - add TensorFlow for Windows/Mac

--- eff_word_net/__init__.py
+++ eff_word_net/__init__.py
@@ -3,9 +3,11 @@
 """
 
 import os
+import sys
 RATE=16000
 samples_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)),"sample_refs")
 
 from eff_word_net.package_installation_scripts import check_install_tflite
 
-check_install_tflite()
+if sys.platform=="linux":
+    check_install_tflite()
